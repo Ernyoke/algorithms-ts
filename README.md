47c445b4ed3a91d8e673f17625150c8270c355bb
Ervin Szilagyi <ernyoke19930115@gmail.com>
2020-04-15 23:05:42 +0300

    Implemented sqrt with Newton method.

--- README.md
+++ README.md
@@ -24,6 +24,7 @@
 
 * Mathematical
     * Is Power Of 2
+    * Square Root - Newton-Raphson method
     
 
 ## Run tests
---
+++ src/math/squareRoot.js
@@ -0,0 +1,33 @@
+/**
+ * Newton-Raphson method
+ * 
+ * x[n + 1] = x[n] - f(x) / f'(x)
+ *   
+ * For finding square root of S we can use f(x) = x^2 - S
+ * so, x[n + 1] = x[n] - (x^2 - S) / 2x
+ * 
+ */
+
+const squareRoot = (number, eps) => {
+    if (number < 0) {
+        throw new Error('Can not get the square root of a negative!');
+    }
+
+    if (number === 0) {
+        return 0;
+    }
+
+    if (!eps) {
+        eps = 0.000001;
+    }
+
+    let x = 1;
+
+    while (Math.abs(x * x - number) > eps) {
+        x = x - (x * x - number) / (2 * x);
+    }
+
+    return x;
+};
+
+module.exports = squareRoot;
