61f5816b34028d80a8a882fb2688eb086903608f
Ervin Szilagyi <ernyoke19930115@gmail.com>
2020-04-18 10:17:26 +0300

    Refactor comment.

--- src/math/isPowerOf2.js
+++ src/math/isPowerOf2.js
@@ -1,20 +1,20 @@
-// Given an integer number find out if it is a power of 2.
-
-// Bitwise solution: number & (number - 1)
-
-// Explanation:
-
-// 1: 0001
-// 2: 0010
-// 4: 0100
-// 8: 1000
-
-
-// Lets take 7:
-
-// 7: 0111
-
-// 8 & (8 - 1) = 1000 & 0111 = 0
+/**
+ * Given an integer number find out if it is a power of 2.
+ *
+ * Bitwise solution: number & (number - 1)
+ *
+ * Explanation:
+ *
+ * 1: 0001
+ * 2: 0010
+ * 4: 0100
+ * 8: 1000
+ * Lets take 7:
+ *
+ * 7: 0111
+ *
+ * 8 & (8 - 1) = 1000 & 0111 = 0 
+ */
 
 const isPowerOf2 = value => (value & (value - 1)) === 0;
 
